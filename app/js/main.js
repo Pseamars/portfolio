7a6cf7f84859749cee9480d6ff6928e750f7a5dd
Patrick <patrick.seamars@gmail.com>
2014-12-07 13:22:54 -0700

    updated styles, added experience content, removed gallery

--- app/js/main.js
+++ app/js/main.js
@@ -37,7 +37,11 @@
 
     I want to give each strength its own button and then have it only update the corresponding strength        
 ********************************************/
-
+//this updates the text only. I wanted to swap out the entire image, but this works. 
+$('#strengths').on('click', '.strengths-item p',function(){
+    var $item = $(this);
+    $item.data( 'swap', [ $item.text(), $item.text( $item.data('swap') ) ][0] );
+});
 
 // $('#strengths').on('click', '.strengths-item', function () {
     
@@ -53,14 +57,6 @@
 
 //     console.log(content);
 // })
-
-$('#strengths').on('click', '.strengths-item p',function(){
-    var $item = $(this);
-    $item.data( 'swap', [ $item.text(), $item.text( $item.data('swap') ) ][0] );
-});
-
-
-
 
     // console.log($item.text())
     // if($item.has(".front")){
@@ -85,10 +81,10 @@ About me
 ********************************************/
 var randomFact = ["Like most Colorado natives, I love to be outside. I am an avid cyclist and even compete with a racing team.", 
     "I am a very curious person and I love to learn new things. Ask me what I'm reading! I am always reading something, and sometimes many things. I am teaching myself how to write web apps and web sites with many different resources, from Code School and textbooks to meetups and friends.", 
-    "I am a foodie. My wife and I are always trying new places to eat in the Denver and Boulder area. I also love to cook. My favorite thing to make is home made spaghetti squash bolagnese", 
-    "I do yoga. It helps keep me centered and feeling strong. Although, I still can't stand on my head", 
-    "I am an advocate for social justice, especially if it concerns education. I am passionate about critical pedagogy and have worked in low income schools. My dream is to create software to help bridge the achievement gap in real and meaningful ways", 
-    "I watch documantaries when I get the chance. It doesn't matter what kind of documentary it is, as long as it is well done. My favorites are social commentaries", 
+    "I am a foodie. My wife and I are always trying new places to eat in the Denver and Boulder area. I also love to cook. My favorite thing to make is home made spaghetti squash bolagnese.", 
+    "I do yoga. It helps keep me centered and feeling strong. Although, I still can't stand on my head.", 
+    "I am an advocate for social justice, especially if it concerns education. I am passionate about critical pedagogy and have worked in low income schools. My dream is to create software to help bridge the achievement gap in real and meaningful ways.", 
+    "I watch documantaries when I get the chance. It doesn't matter what kind of documentary it is, as long as it is well done. My favorites are social commentaries.", 
     "I try to stay healthy, whether that is through the food I eat or the activities I take part in. I also try not to preach about it too. No one likes that guy."
     ]
 
